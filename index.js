f19ef3c40d848ab87fdcd3993d85c8aa098ad481
Sucheta Pal <pal.sucheta1@gmail.com>
2022-03-04 00:53:27 +0530

    set up the view engine

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 8000;
 
 // use express router
 app.use('/',require('./routes'))
+
+// setting the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,(err)=>{
     if(err)
     {
