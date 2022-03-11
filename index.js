b4085db5d16884c6aa9f1ed495b530990a656c24
Sucheta Pal <pal.sucheta1@gmail.com>
2022-03-11 19:40:04 +0530

    Style the todo app

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use('/',require('./routes'))
 // setting the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+app.use(express.urlencoded());
+app.use(express.static('assets'));
 app.listen(port,(err)=>{
     if(err)
     {
